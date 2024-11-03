9cf5f5c856961e02d735fef0b11e228df1480c71
Timothy Lee <timothylhy@hotmail.com>
2024-11-03 16:35:11 +0800

    Added get route with redis cache

---
+++ src/app/[shortCode]/route.ts
@@ -0,0 +1,48 @@
+import { RESERVED_PATHS } from "@/lib/constants";
+import prisma from "@/lib/db";
+import { Redis } from "@upstash/redis";
+import { NextResponse } from "next/server";
+
+const redis = Redis.fromEnv();
+
+export async function GET(
+  request: Request,
+  {
+    params,
+  }: {
+    params: {
+      shortCode: string;
+    };
+  }
+) {
+  const { shortCode } = params;
+
+  if (RESERVED_PATHS.includes(shortCode)) {
+    return NextResponse.next();
+  }
+
+  if (await redis.exists(shortCode)) {
+    const originalUrl = await redis.get<string>(shortCode);
+
+    if (originalUrl) {
+      return NextResponse.redirect(originalUrl, 302);
+    }
+  }
+
+  const result = await prisma.url.findUnique({
+    where: {
+      shortCode,
+    },
+    select: {
+      url: true,
+    },
+  });
+
+  if (!result) {
+    return NextResponse.json({ error: "Not found" }, { status: 404 });
+  }
+
+  await redis.set(shortCode, result.url);
+
+  return NextResponse.redirect(result.url, 302);
+}
